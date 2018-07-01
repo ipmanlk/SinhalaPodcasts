33c5eb805b4c726bc274079a48474dd2ca7a7711
ipman98 <navilk@zoho.com>
2018-07-01 16:37:12 +0530

    add: offline check

--- www/js/index.js
+++ www/js/index.js
@@ -293,3 +293,9 @@ function backgroundMode() {
     cordova.plugins.backgroundMode.excludeFromTaskList();
 }
 
+//check online status
+document.addEventListener("offline", onOffline, false);
+
+function onOffline() {
+    alert("You are offline!. Please connect to the Internet!.");
+}
