3d493dc0c008d24f4a41ab42b991bac6d990dd84
ipman98 <navilk@zoho.com>
2018-07-03 09:43:03 +0530

    add: new podcast channels

--- www/js/app/getAssets.js
+++ www/js/app/getAssets.js
@@ -3,30 +3,27 @@ function getLogo(source) {
     switch (source) {
         case "TechKatha":
             return ('./img/logo/TechKatha.png');
-            break;
         case "Illumination Cast":
             return ('./img/logo/IlluminationCast.png');
-            break;
         case "HyperStella":
             return ('./img/logo/HyperStella.png');
-            break;
         case "MindHorizon":
             return ('./img/logo/MindHorizon.png');
-            break;
         case "Daylight Podcast":
             return ('./img/logo/DaylightPodcast.png');
-            break;
         case "Micro Science":
             return ('./img/logo/MicroScience.png');
-            break;
         case "MCUSL-Talk":
             return ('./img/logo/MCUSL-Talk.png');
-            break;
         case "Sanhinda Podcast":
             return ('./img/logo/SanhindaPodcast.png');
-            break;
         case "Binary Podcast":
             return ('./img/logo/BinaryPodcast.png');
-            break;
+        case "Music.lk":
+            return ('./img/logo/Music.lk.png');
+        case "Topic Eka Podcast":
+            return ('./img/logo/TopicEkaPodcast.png');
+        default:
+            return ('./img/logo/Default.png');
     }
 }
